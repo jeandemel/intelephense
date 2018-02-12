9c9d2016a6757888e6d37997332323be23f6c6b4
Ben Mewburn <ben@mewburn.id.au>
2018-02-12 23:38:55 +1100

    transform refactor WIP

---
+++ src/transforms/typesPass.ts
@@ -0,0 +1,14 @@
+/* Copyright (c) Ben Robert Mewburn
+ * Licensed under the ISC Licence.
+ */
+
+'use strict';
+
+
+/** 
+ * Second parse tree pass.
+ * 1. Add type info to variables, call expr, array creation expr 
+ */
+class SecondPass implements TreeVisitor<Phrase | Token> {
+
+}
